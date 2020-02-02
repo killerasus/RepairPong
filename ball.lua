a2c5d507bf4dc6dd925269ad46906034c621c037
Bruno Baère Pederassi Lomba de Araujo <killerasus@gmail.com>
2020-02-02 10:16:44 -0300

    Ball paddle bump randomizer

--- ball.lua
+++ ball.lua
@@ -55,12 +55,18 @@ function Ball:update( dt )
 end
 
 function Ball:checkPlayerCollision( player )
-	speed_x, speed_y = self:getSpeed()
-	player_x, player_y = player:getPosition()
-	width, height = player:getDimensions()
+	local speed_x, speed_y = self:getSpeed()
+	local player_x, player_y = player:getPosition()
+	local width, height = player:getDimensions()
 
 	if self.x + self.size > player_x and self.x < player_x + width and
 	    self.y + self.size > player_y and self.y < player_y + height then
+
+    	-- From: https://gamedev.stackexchange.com/questions/4253/in-pong-how-do-you-calculate-the-balls-direction-when-it-bounces-off-the-paddl
+	   	local d = player_y + height/2 - (self.y)
+
+    	speed_y = speed_y + d*(-0.1)
+
 	    if (speed_x < 0) then 
 	    	speed_x = -(speed_x - self.speed_increment)
 	    	self.x = player_x + width;
@@ -68,6 +74,7 @@ function Ball:checkPlayerCollision( player )
 	    	speed_x = -(speed_x + self.speed_increment)
 	    	self.x = player_x - self.size
     	end
+
     	player:applyDamage( self.computeDamage() )
 	end
 
